1e201730c998904ae4934a6c5f542b4394a6c4f6
AmeeHmza <global.hamzagc@gmail.com>
2022-08-22 00:25:50 -0700

    1st commit

--- App.js
+++ App.js
@@ -1,64 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './src/screens/Home';
+import About from './src/screens/About';
+import Contact from './src/screens/Contact';
+import Course from './src/screens/Course';
+import UserData from './src/screens/UserData';
 
 export default function App() {
-  const description = 
-  "Lorem ipsum dolor site amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.";
+  const Stack = createNativeStackNavigator();
 
   return (
-    <View style={styles.mainContainer}>
-       <View style={styles.homeTop}>
-       <Image
-       style={styles.headerImage}
-       resizeMode="contain"
-       source={require("./assets/logo.jpg")}
-       />
-       <Text style={styles.mainHeader}>Welcome to </Text>
-       <Text style={[styles.mainHeader,
-        {
-          fontSize:33, 
-          color: "#4c5dab",
-          marginTop:0,
-        }]}>
-        educative system</Text>
-        <Text style={styles.paraStyle}>{description} </Text>
-       </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        {/* home screen */}
+        <Stack.Screen name="Home">
+          {props => <Home {...props} channelName={'educative system'} />}
+        </Stack.Screen>
+
+        {/* Course screen */}
+        <Stack.Screen name="Course" component={Course} />
+
+        {/* About screen */}
+        <Stack.Screen name="About" component={About} />
+
+        {/* UserData screen */}
+        <Stack.Screen name="Student" component={UserData} />
+
+        {/* Contact screen */}
+        <Stack.Screen name="Contact" component={Contact} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  mainContainer:{
-    height: "100%",
-    display: "flex",
-    justifyContent: "space-between",
-    paddingHorizontal: 20,
-    backgroundColor: "#fff",
-    alignItems: "center",
-  },
-  homeTop: {
-    // height: "100%",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: 10,
-  },
-
-  headerImage: {
-    height: undefined,
-    width: "100%",
-    aspectRatio: 1,
-    display: "flex",
-    alignItems: "stretch",
-    marginTop: 50,
-    borderRadius: 20,
-
-  },
-  mainHeader: {
-  fontSize: 30,
-  color: "#344055",
-  textTransform: "uppercase",
-  
-  },
-});
---
+++ src/component/Menu.js
@@ -0,0 +1,91 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {TouchableOpacity, View, StyleSheet, Text,Image} from 'react-native';
+
+const Menu = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.menuContainer}>
+
+      <TouchableOpacity
+        style={[styles.buttonStyle,
+          {
+            paddingHorizontal:25
+          },
+          ]}
+        onPress={() => navigation.navigate('Course')}>
+        {/* <Text>Course</Text> */}
+        <Image 
+        style={styles.iconStyle}
+        source={{
+          uri: "https://img.icons8.com//stickers/90/000000/training.png",
+        }}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.buttonStyle,
+          {
+            paddingHorizontal:25
+          },
+        ]}
+        onPress={() => navigation.navigate('Student')}>
+       {/*  <Text>Student</Text> */} 
+       <Image 
+        style={styles.iconStyle}
+        source={{
+          uri: "https://img.icons8.com/stickers/100/000000/conference.png",
+        }}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      style={[styles.buttonStyle,
+        {
+          paddingHorizontal:25
+        },
+      ]}
+        onPress={() => navigation.navigate('About')}>
+      {/*    <Text>About</Text> */}
+      <Image 
+        style={styles.iconStyle}
+        source={{
+          uri: "https://img.icons8.com/stickers/100/000000/about.png",
+        }}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      style={[styles.buttonStyle,
+        {
+          paddingHorizontal:25
+        },
+      ]}
+        onPress={() => navigation.navigate('Contact')}>
+      {/*  <Text>Contact</Text>  */} 
+      <Image 
+        style={styles.iconStyle}
+        source={{
+          uri: "https://img.icons8.com/stickers/100/000000/phone-office.png",
+        }}/>
+      </TouchableOpacity>
+
+    
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  menuContainer: {
+        flexDirection: "row",
+    },
+    textStyle: {
+        textTransform: "uppercase",
+    },
+    iconStyle: {
+        width: "100%",
+        height: 50,
+        aspectRatio: 1,
+    },
+    
+});
+
+export default Menu;
